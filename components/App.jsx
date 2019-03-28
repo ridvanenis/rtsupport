1273abf750b0e485221bc38f0724699aa65fc2fa
Ridvan ENIS <renis@netes.com.tr>
2019-03-28 23:11:05 +0300

    add UserSeaction Components

--- components/App.jsx
+++ components/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ChannelSection from './channels/ChannelSection.jsx';
+import UserSection from './users/UserSection.jsx';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             channels: [],
-            activeChannel: {}
+            activeChannel: {},
+            users: []
         }
     }
 
@@ -14,12 +16,18 @@ class App extends Component {
         let { channels } = this.state;
         channels.push({ id: channels.length, name });
         this.setState({ channels });
-        
+
         // TODO: Send to server
     }
 
     setChannel(activeChannel) {
         this.setState({ activeChannel })
+    }
+
+    setUserName(name){
+        let { users } = this.state;
+        users.push({id: users.length, name});
+        this.setState({users});
     }
 
     render() {
@@ -30,6 +38,11 @@ class App extends Component {
                         {...this.state}
                         addChannel={this.addChannel.bind(this)}
                         setChannel={this.setChannel.bind(this)}
+                    />
+
+                    <UserSection
+                        {...this.state}
+                        setUserName={this.setUserName.bind(this)}
                     />
                 </div>
             </div>
