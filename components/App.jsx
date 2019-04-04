93788780abac0dfeaa98ffb063b46e4016a4c434
Ridvan ENIS <renis@netes.com.tr>
2019-04-04 22:46:32 +0300

    add websocket

--- components/App.jsx
+++ components/App.jsx
@@ -10,15 +10,51 @@ class App extends Component {
             channels: [],
             activeChannel: {},
             users: [],
-            messages: []
+            messages: [],
+            connected:  false
         }
+    }
+
+    componentDidMount(){
+        let ws = this.ws = new WebSocket('wss://echo.websocket.org/');
+        ws.onmessage = this.message.bind(this);
+        ws.onopen = this.open.bind(this);
+        ws.onclose = this.close.bind(this);
+    }
+
+    message(e){
+        const event = JSON.parse(e.data);
+        if(event.name === 'channel add'){
+            this.newChannel(event.data);
+        }
+    }
+
+    open(){
+        this.setState({connected : true});
+    }
+
+    close(){
+        this.setState({connected : false});
+    }
+
+    newChannel(channel){
+        let {channels} = this.state;
+        channels.push(channel);
+        this.setState(channels);
     }
 
     addChannel(name) {
         let { channels } = this.state;
-        channels.push({ id: channels.length, name });
-        this.setState({ channels });
 
+        let msg = {
+            name: 'channel add',
+            data :{
+                id: channels.length,
+                name
+            }
+        }
+
+        this.ws.send(JSON.stringify(msg));
         // TODO: Send to server
     }
 
