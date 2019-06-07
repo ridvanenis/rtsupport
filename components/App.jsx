d1f3d2f4736dc18d3ea56cfc035ff5fcae59fc36
Ridvan ENİŞ <ridvanenis@gmail.com>
2019-06-07 12:34:56 +0300

    connect rtsupport-go project

--- components/App.jsx
+++ components/App.jsx
@@ -18,7 +18,8 @@ class App extends Component {
     }
 
     componentDidMount(){
-        let socket = this.socket = new Socket();
+        let ws = new WebSocket('ws://localhost:4000')
+        let socket = this.socket = new Socket(ws);
         socket.on('connect', this.onConnect.bind(this));
         socket.on('disconnect', this.onDisconnect.bind(this));
         socket.on('channel add', this.onAddChannel.bind(this));
